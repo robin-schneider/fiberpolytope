573d78f3e18337ff7d74591fc42b995acec5768a
Robin Schneider <robinschneider93@gmail.com>
2021-02-01 14:05:08 +0100

    updated setup, name + dependencies

--- setup.py
+++ setup.py
@@ -4,20 +4,21 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="WSFpolytope",
+    name="fiberpolytope",
     version="0.1",
     author="Robin Schneider",
     author_email="robin.schneider@physics.uu.se",
     description="A sage package to study fibrations of reflexive polytopes and their F-theroy compactifications.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/robin-schneider/WSFpolytope",
+    url="https://github.com/robin-schneider/fiberpolytope",
     packages=setuptools.find_packages(),
-    data_files=[('sage', ['WSFpolytope/WSFpolytope.sage']
+    data_files=[('sage', ['fiberpolytope/fiberpolytope.sage']
     )],
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering :: Mathematics",
@@ -25,6 +26,7 @@ setuptools.setup(
     ],
     install_requires=[
         "numpy",
-        "pandas",
+	"matplotlib",
+	"scipy"
     ],
 )
